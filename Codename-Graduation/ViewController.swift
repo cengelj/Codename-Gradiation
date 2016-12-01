034ae0e7c8558e4e8e742fcf014bc8f17203362e
Joseph Cengel (student LM) <s011878@lm-18s011878.lmsd.org>
2016-12-01 08:36:37 -0500

    Initial Commit

---
+++ Codename-Graduation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codename-Graduation
+//
+//  Created by Joseph Cengel (student LM) on 12/1/16.
+//  Copyright © 2016 Joseph Cengel (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
