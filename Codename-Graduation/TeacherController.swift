8f418689bd172bd373ab735bb3fe1ed83ad0a651
Joseph Cengel (student LM) <s011878@lm-18s011878.lmsd.org>
2016-12-08 10:50:29 -0500

    Added view controllers for the other two sections, connected them, and have them using a custom class. Need to implement functionality later as they currently just have a centered label with a name.

---
+++ Codename-Graduation/TeacherController.swift
@@ -0,0 +1,13 @@
+//
+//  TeacherController.swift
+//  Codename-Graduation
+//
+//  Created by Joseph Cengel (student LM) on 12/8/16.
+//  Copyright © 2016 Joseph Cengel (student LM). All rights reserved.
+//
+
+import UIKit
+
+class TeacherController: UIViewController{
+	
+}
