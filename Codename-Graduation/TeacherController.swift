38ccf8ecf9404d3ec7b350154d379680292fda7f
Joseph Cengel (student LM) <s011878@lm-18s011878.lmsd.org>
2017-01-12 09:17:15 -0500

    Started teacher viewer

--- Codename-Graduation/TeacherController.swift
+++ Codename-Graduation/TeacherController.swift
@@ -24,14 +24,17 @@ class TeacherController: UIViewController, UITableViewDataSource, UITableViewDel
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let tabRet = UITableViewCell()
-		tabRet.textLabel!.font = UIFont(name: "helvetica neue", size: 20)
-		tabRet.textLabel?.textAlignment = .center
+		tabRet.textLabel!.font = UIFont(name: "helvetica neue", size: 15)
+		tabRet.textLabel?.textAlignment = .left
 		tabRet.textLabel?.text = teachers[indexPath.row].name
 		
 		return tabRet
 	}
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return teachers.count
+	}
+	func tableView(_: UITableView, didSelectRowAt: IndexPath){
+		
 	}
 	func addTeacher(name: String, compHW: Bool, testDiff: Bool, testCurve: Bool, strict: Float){
 		var add = Teacher(name: name, compHW: compHW, testDiff: testDiff, testCurve: testCurve, strict: strict)
